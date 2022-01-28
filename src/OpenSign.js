857ebe03263724d4401972f2b2da560a370d3371
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-28 09:33:55 -0800

    added OpenSign.css

--- src/OpenSign.js
+++ src/OpenSign.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './OpenSign.css';
 
 export default function OpenSign({ isOpen }) {
-  return <div>
+  return <div className="open-sign">
     {isOpen ? 'Come on in!' : 'Sorry, the zoo is closed.'}
   </div>;
 }
