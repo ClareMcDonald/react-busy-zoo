5bb2b33d35f8f86af82b78658bc1ef86063c3761
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-27 17:10:44 -0800

    unicorn and dragon buttons and styling added for the fight

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 
 function App() {
 
-  const [unicornSize, setUnicornSize] = useState[1];
-  const [lionSize, setLionSize] = useState[1];
+  const [unicornSize, setUnicornSize] = useState(1);
+  const [dragonSize, setDragonSize] = useState(1);
 
   return (
     <div className="App">
       <div className="fight">
-        <div className="lion">
-          <p></p>
-          <button onClick={() => setUnicornSize(unicornSize + 1)}></button>
+        <div className="unicorn">
+          <p style={{ fontSize: `${unicornSize}rem` }}>🦄</p>
+          <button onClick={() => setUnicornSize(unicornSize + 1)}>Unicorn is eating magic sparkles. He is growing so large!</button>
+          <button onClick={() => setDragonSize(dragonSize - 1)}>Unicorn hit Dragon!</button>
+        </div>
+        <div className="dragon">
+          <p style={{ fontSize: `${dragonSize}rem` }}>🐲</p>
+          <button onClick={() => setDragonSize(dragonSize + 1)}>Dragon is drinking from the fire well. She is getting stronger!</button>
+          <button onClick={() => setUnicornSize(unicornSize - 1)}>Dragon hit Unicorn!</button>
         </div>
       </div>
     </div>
