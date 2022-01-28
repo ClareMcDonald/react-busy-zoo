439deb8087aec615ab5bd409844225c919b09420
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-27 16:37:44 -0800

    start work on app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [unicornSize, setUnicornSize] = useState[1];
+  const [lionSize, setLionSize] = useState[1];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="fight">
+        <div className="lion">
+          <p></p>
+          <button onClick={() => setUnicornSize(unicornSize + 1)}></button>
+        </div>
+      </div>
     </div>
   );
 }
